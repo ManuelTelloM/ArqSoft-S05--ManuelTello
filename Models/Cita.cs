cb709efb539faad973bc36bc8c9eae79e2f771ff
Manuel Jesus Tello May <manuel.tello@tecdesoftware.edu.mx>
2026-06-02 18:42:37 -0600

    Agregar archivos de proyecto.

---
+++ Models/Cita.cs
@@ -0,0 +1,15 @@
+﻿namespace Citas_App.Models
+{
+    public class Cita
+    {
+        public int Id { get; set; }
+        public int PacienteId { get; set; }
+        public int MedicoId { get; set; }
+
+        public DateOnly Fecha { get; set; }
+        public TimeOnly Hora { get; set; }
+        public String Motivo { get; set; }
+        public String Estado { get; set; }
+
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,14 @@
+﻿namespace Citas_App.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+
+        public String Email { get; set; }
+        public String Telefono { get; set; }
+
+
+    }
+}
