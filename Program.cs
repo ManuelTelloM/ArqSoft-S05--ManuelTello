0063a6c67feac58f72260d274a820cd26c3eb1a0
Manuel Jesus Tello May <manuel.tello@tecdesoftware.edu.mx>
2026-06-16 22:16:45 -0600

    FEAT: Correccion y arreglo de códigos en la rama hexagonal

--- Program.cs
+++ Program.cs
@@ -1,29 +1,51 @@
+using Citas_App.Interfaces;
+using Citas_App.Repositories;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Carpeta donde se guardan los archivos CSV.
+var dataFolder = Path.Combine(builder.Environment.WebRootPath, "data");
+Directory.CreateDirectory(dataFolder);
+
+var csvPacientes = Path.Combine(dataFolder, "pacientes.csv");
+var csvMedicos = Path.Combine(dataFolder, "medicos.csv");
+var csvCitas = Path.Combine(dataFolder, "citas.csv");
+
+var sqlitePath = Path.Combine(dataFolder, "citasapp.db");
+
 builder.Services.AddControllersWithViews();
+
+// JSON
+// builder.Services.AddScoped<IPacienteRepository, JsonPacienteRepository>();
+// builder.Services.AddScoped<IMedicoRepository, JsonMedicoRepository>();
+// builder.Services.AddScoped<ICitaRepository, JsonCitaRepository>();
+
+// CSV
+builder.Services.AddSingleton<IPacienteRepository>(_ => new CsvPacienteRepository(csvPacientes));
+builder.Services.AddSingleton<IMedicoRepository>(_ => new CsvMedicoRepository(csvMedicos));
+builder.Services.AddSingleton<ICitaRepository>(_ => new CsvCitaRepository(csvCitas));
+
+// SQLite
+// builder.Services.AddSingleton<IPacienteRepository>(_ => new SqlitePacienteRepository(sqlitePath));
+// builder.Services.AddSingleton<IMedicoRepository>(_ => new SqliteMedicoRepository(sqlitePath));
+// builder.Services.AddSingleton<ICitaRepository>(_ => new SqliteCitaRepository(sqlitePath));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
